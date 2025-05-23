dec9552a083e586f97d17a222b9d74872bbaeb47
kapil j <jkapilraj@kapils-MacBook-Air.local>
2025-05-23 14:17:40 -0400

    first commit

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ProductList } from './index';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <ProductList products={[{ id: 1, name: 'Product A', price: 100 }]} />
